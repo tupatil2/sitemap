867559681c0709bffabdab5459b2220e9d247855
tusharr-patil <patiltusharvikas@gmail.com>
2023-07-15 19:36:32 +0530

    add download as xml feature

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -22,18 +23,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// func handlerSubmit(w http.ResponseWriter, r *http.Request) {
+// 	siteName = r.FormValue("siteName")
+// 	log.Println("site name is receive", siteName)
+// 	pages := 10
+
+// 	xmlResponse := Sitemap.GenerateSiteMap(siteName, pages)
+// 	resp := Response{XMLResponse: xmlResponse}
+// 	tmpl := template.Must(template.ParseFiles("tmpl/main.page.tmpl"))
+// 	err := tmpl.Execute(w, resp)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+// }
+
 func handlerSubmit(w http.ResponseWriter, r *http.Request) {
 	siteName = r.FormValue("siteName")
-	log.Println("site name is receive", siteName)
+	log.Println("site name is received:", siteName)
 	pages := 10
 
 	xmlResponse := Sitemap.GenerateSiteMap(siteName, pages)
-	resp := Response{XMLResponse: xmlResponse}
-	tmpl := template.Must(template.ParseFiles("tmpl/main.page.tmpl"))
-	err := tmpl.Execute(w, resp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	fmt.Println(xmlResponse)
+	// Set the response content type as plain text
+	w.Header().Set("Content-Type", "text/xml")
+
+	// Write the XML response
+	w.Write([]byte(xmlResponse))
 }
 
 func main() {
